e1315ab810d084d03f913920850d5e84a2ee6bac
bobby.li <lihan0603@hupu.com>
2019-06-05 14:28:36 +0800

    build/version

--- build/version.js
+++ build/version.js
@@ -1,27 +1,57 @@
 const fs = require('fs')
 const pkg = fs.readFileSync('./package.json', 'utf-8')
+const { argv } = process
+const versionRegExp = /^\d+\.\d+\.\d+$/
 const normalRegExp = /version.*(\d+).(\d+).(\d+)/
 const betaRegExp = /version.*(\d+).(\d+).(\d+)\-(beta).(\d+)/
+const isBeta = argv[2] === 'beta'
 const match = pkg.match(betaRegExp)
+const argvV = isBeta ? argv[3] : argv[2]
+const manualV = versionRegExp.test(argvV) && argvV
 
 let version = ''
-if (match) {
-  version = `version": "${match[1]}.${match[2]}.${match[3]}",`
-} else {
-  const match = pkg.match(normalRegExp)
-  if (match[2] >= 99 && match[3] >= 99) {
-    match[1] = parseInt(match[1]) + 1
-    match[2] = 0
-    match[3] = 0
+if (isBeta) {
+  if (match) {
+    version = `version": "${manualV || `${match[1]}.${match[2]}.${match[3]}-beta.${+match[5] + 1}`}",`
   } else {
-    if (match[3] >= 99) {
-      match[2] = parseInt(match[2]) + 1
+    const match = pkg.match(normalRegExp)
+    if (match[2] >= 99 && match[3] >= 99) {
+      match[1] = parseInt(match[1]) + 1
+      match[2] = 0
       match[3] = 0
     } else {
-      match[3] = parseInt(match[3]) + 1
+      if (match[3] >= 99) {
+        match[2] = parseInt(match[2]) + 1
+        match[3] = 0
+      } else {
+        match[3] = parseInt(match[3]) + 1
+      }
+    }
+    version = `version": "${match[1]}.${match[2]}.${match[3]}-beta.${manualV || 0}",`
+  }
+} else {
+  if (match) {
+    version = `version": "${manualV || `${match[1]}.${match[2]}.${match[3]}`}",`
+  } else {
+    if (manualV) {
+      version = `version": "${manualV}",`
+    } else {
+      const match = pkg.match(normalRegExp)
+      if (match[2] >= 99 && match[3] >= 99) {
+        match[1] = parseInt(match[1]) + 1
+        match[2] = 0
+        match[3] = 0
+      } else {
+        if (match[3] >= 99) {
+          match[2] = parseInt(match[2]) + 1
+          match[3] = 0
+        } else {
+          match[3] = parseInt(match[3]) + 1
+        }
+      }
+      version = `version": "${match[1]}.${match[2]}.${match[3]}",`
     }
   }
-  version = `version": "${match[1]}.${match[2]}.${match[3]}",`
 }
 
 fs.writeFileSync('./package.json', pkg.replace(/version.*,/, version))
