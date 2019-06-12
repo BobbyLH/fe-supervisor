aa8fa259add9bcd3145b40aad1e710e4adf2bcf5
bobby.li <lihan0603@hupu.com>
2019-06-12 15:13:48 +0800

    [FE - SUPERVISOR]: "version": "0.0.13"

--- src/utils/notSupport.ts
+++ src/utils/notSupport.ts
@@ -1,11 +1,9 @@
-import { notSupportPromisify } from '../index.d'
-
 export function notSupport (): false {
   console.warn('Your browser not support the [Performance] API!')
   return false
 }
 
-export function notSupportPromisify (): notSupportPromisify {
+export function notSupportPromisify (): Promise<false> {
   console.warn('Your browser not support the [Performance] API!')
   return Promise.resolve(false)
 }
